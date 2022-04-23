930a6519dc51bb781a4ee5234e4376775bb4d376
LimCherry <cherry523208@gmail.com>
2022-04-23 15:51:47 +0900

    5
    animator save

--- MagicSchool_005/Assets/Scripts/PotEvent.cs
+++ MagicSchool_005/Assets/Scripts/PotEvent.cs
@@ -1,6 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+
+public class Particle : MonoBehaviour
+{
+    //public bool playAura = true; //파티클 제어 bool
+    public ParticleSystem particleObject; //파티클시스템
+}
 
 public class PotEvent : MonoBehaviour
 {
@@ -8,52 +15,136 @@ public class PotEvent : MonoBehaviour
     public GameObject Potion_R;
     public GameObject Potion_G;
     public GameObject Potion_Y;
+
     public GameObject Cube;
+    public GameObject Capsule1;
+    public GameObject Cylinder;
+    public GameObject Sphere;
+    public GameObject Capsule2;
+    public GameObject Cube2;
 
-
+    public ParticleSystem Pongdang; //파티클시스템
+    public ParticleSystem Bubble; //파티클시스템
 
     private Collider coll;
+    private List<string> tagList = new List<string>();
+    private int cnt = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         Cube.SetActive(false);
+        Capsule1.SetActive(false);
+        Cylinder.SetActive(false);
+        Sphere.SetActive(false);
+        Capsule2.SetActive(false);
+        Cube2.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider coll)
     {
         if (coll.gameObject == Potion_B)
         {
+            cnt += 1;
             Potion_B.SetActive(false);
+            Pongdang.startColor = new Color(0, 50, 255);
+            Pongdang.Play();
+            tagList.Add("Blue");
         }
 
         if (coll.gameObject == Potion_R)
         {
+            cnt += 1;
             Potion_R.SetActive(false);
+            Pongdang.startColor = new Color(255, 0, 0);
+            Pongdang.Play();
+            tagList.Add("Red");
         }
 
         if (coll.gameObject == Potion_G)
         {
+            cnt += 1;
             Potion_G.SetActive(false);
+            Pongdang.startColor = new Color(0, 128, 0);
+            Pongdang.Play();
+            tagList.Add("Green");
         }
 
         if (coll.gameObject == Potion_Y)
         {
+            cnt += 1;
             Potion_Y.SetActive(false);
+            Pongdang.startColor = new Color(255, 215, 0);
+            Pongdang.Play();
+            tagList.Add("Yellow");
         }
 
-        if (coll.gameObject == Potion_B 
-            && coll.gameObject == Potion_R)
+        if (cnt == 2)
         {
-            Cube.SetActive(true);
+            //Debug.Log("tagList : " + tagList[0] + ", " + tagList[1]);
+            Invoke("DragonAppear", 2f);
         }
-
     }
 
-    /*
-    // Update is called once per frame
-    void Update()
+    public void DragonAppear()
     {
-        
-    }*/
+        Debug.Log("tagList : " + tagList[0] + ", " + tagList[1]);
+        Bubble.Play();
+
+        if (tagList.Contains("Red") && tagList.Contains("Blue"))
+        {
+            StartCoroutine(cubeActive());
+        }
+        if (tagList.Contains("Red") && tagList.Contains("Green"))
+        {
+            StartCoroutine(capsule1Active());
+        }
+        if (tagList.Contains("Red") && tagList.Contains("Yellow"))
+        {
+            StartCoroutine(CylinderActive());
+        }
+        if (tagList.Contains("Blue") && tagList.Contains("Green"))
+        {
+            StartCoroutine(SphereActive());
+        }
+        if (tagList.Contains("Blue") && tagList.Contains("Yellow"))
+        {
+            StartCoroutine(Capsule2Active());
+        }
+        if (tagList.Contains("Yellow") && tagList.Contains("Green"))
+        {
+            StartCoroutine(Cube2Active());
+        }
+    }
+
+    IEnumerator cubeActive()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Cube.SetActive(true);
+    }
+    IEnumerator capsule1Active()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Capsule1.SetActive(true);
+    }
+    IEnumerator CylinderActive()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Cylinder.SetActive(true);
+    }
+    IEnumerator SphereActive()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Sphere.SetActive(true);
+    }
+    IEnumerator Capsule2Active()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Capsule2.SetActive(true);
+    }
+    IEnumerator Cube2Active()
+    {
+        yield return new WaitForSeconds(1.0f);
+        Cube2.SetActive(true);
+    }
 }
---
+++ MagicSchool_005/Assets/Scripts/chatController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class chatController : MonoBehaviour
+{
+    public XRController controller = null;
+
+    public Text ChatText;
+    public string writerText = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TextPractice());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator NormalChat(string narration)
+    {
+        int a = 0;
+        writerText = "";
+
+        //텍스트 타이핑 효과
+        for (a = 0; a < narration.Length; a++)
+        {
+            writerText += narration[a];
+            ChatText.text = writerText;
+            yield return null;
+        }
+
+        //키를 다시 누를때까지 무한정 대기
+        while (true)
+        {
+            controller.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool AButton);
+
+            if (AButton)
+            {
+                Debug.Log("Press A");
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator TextPractice()
+    {
+        yield return StartCoroutine(NormalChat("여기 4개의 마법약이 보이지?"));
+        yield return StartCoroutine(NormalChat("원하는 마법약 2개를 선택해 마법의 항아리에 하나씩 넣어봐."));
+        yield return StartCoroutine(NormalChat("너만의 드래곤이 나타날거야!"));
+        yield return StartCoroutine(NormalChat("(Grip을 이용해 마법약을 잡고 항아리에 넣어보세요. 손을 놓으면 다시 선택할 수 있습니다.)"));
+    }
+}
